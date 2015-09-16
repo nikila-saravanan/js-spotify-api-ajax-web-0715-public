1571dcae8f39bbfc9bf022bf2d67b4125f27a8a1
nikila-saravanan <nikila.saravanan@gmail.com>
2015-09-15 20:02:02 -0400

    done

--- js/spotify-chart.js
+++ js/spotify-chart.js
@@ -1,10 +1,10 @@
 var url = "http://charts.spotify.com/api/tracks/most_streamed/us/weekly/latest";
 
 var dataSetProperties = {
-  label: 'Spotify Chart of Top 20 Streamed Songs on Spotify with their Steam Count', 
-  fillColor: 'rgba(220,220,220,0.5)', 
-  strokeColor: 'rgba(220,220,220,0.8)', 
-  highlightFill: 'rgba(220,220,220,0.75)', 
+  label: 'Spotify Chart of Top 20 Streamed Songs on Spotify with their Steam Count',
+  fillColor: 'rgba(220,220,220,0.5)',
+  strokeColor: 'rgba(220,220,220,0.8)',
+  highlightFill: 'rgba(220,220,220,0.75)',
   highlightStroke: 'rgba(220,220,220,1)'
 }
 
@@ -13,29 +13,49 @@ $(function() {
 });
 
 function extractTop20Tracks(tracks) {
-  // your code here
+  return tracks.slice(0,20);
+
 }
 
 function extractNumberOfStreams(tracks) {
-  // your code here
+  var streams = []
+  tracks.forEach(function(track) {
+    streams.push(track.num_streams);
+  });
+  return streams
 }
 
 function extractNames(tracks) {
+  var names = []
+  tracks.forEach(function(track) {
+    names.push(track.track_name);
+  });
+  return names
   // your code here
 }
 
 function chartData(labels, inputData) {
   // your code here
-
+  return {labels: labels,
+    datasets: [{
+      label: 'Spotify Chart of Top 20 Streamed Songs on Spotify with their Steam Count',
+      fillColor: 'rgba(220,220,220,0.5)',
+      strokeColor: 'rgba(220,220,220,0.8)',
+      highlightFill: 'rgba(220,220,220,0.75)',
+      highlightStroke: 'rgba(220,220,220,1)',
+      data: inputData
+    }]
+  };
   // use the dataSetProperties variable defined above if it helps
 }
 
 function getSpotifyTracks(callback){
-  // your ajax call here, on success it should call on the 
-  // parameter it's passed (it's a function), and pass it's 
-  // parameter the data it received
-
-  // use the url variable defined above if it helps
+  $.ajax({
+    url: 'http://charts.spotify.com/api/tracks/most_streamed/us/weekly/latest',
+    dataType: 'jsonp',
+    success: function(response) {
+    }
+  });
 }
 
 function success(parsedJSON) {
@@ -49,4 +69,10 @@ function success(parsedJSON) {
   //  5. make a variable `ctx` and select the canvas with the id of spotify-chart
   //     * also make sure to specify 2d context
   //  6. make a new bar chart!
+  var tracks = extractTop20Tracks(parsedJSON.tracks);
+  var names = extractNames(tracks);
+  var streams = extractNumberOfStreams(tracks);
+  var data = chartData(names, streams);
+  var ctx = document.getElementByID('#spotify-chart').getContext('2d');
+  new Chart(ctx).Bar(data);
 }
